2b96f1b5ae622736e74b2f8f8f163a4a3a2c8dcc
Lieven Menschaert <lievenmenschaert@gmail.com>
2019-10-10 15:19:06 +0200

    Add noise fx admin page

--- src/Admin.js
+++ src/Admin.js
@@ -9,23 +9,25 @@ export default class Admin extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <h4>Add image/video</h4>
-        <div className="form">
-          <label id="lab" htmlFor="file">
-            upload file:
-          </label>
-          <input type="file" id="file" />
-          <br />
-          <label id="lab" htmlFor="sel">
-            type:
-          </label>
-          <select id="sel">
-            <option value="image">image</option>
-            <option value="video">video</option>
-          </select>
-          <br />
-          <button>send</button>
+      <div className="noise">
+        <div className="container">
+          <h4>Add image/video</h4>
+          <div className="form">
+            <label id="lab" htmlFor="file">
+              upload file:
+            </label>
+            <input type="file" id="file" />
+            <br />
+            <label id="lab" htmlFor="sel">
+              type:
+            </label>
+            <select id="sel">
+              <option value="image">image</option>
+              <option value="video">video</option>
+            </select>
+            <br />
+            <button>send</button>
+          </div>
         </div>
       </div>
     );
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Player from "./Player";
 import Admin from "./Admin";
+import "./App.css";
 
 export default class App extends React.Component {
   render() {
