3cc12e5d20ce94a29324ca597fdff435524b2a27
Lieven Menschaert <lievenmenschaert@gmail.com>
2019-10-15 14:48:58 +0200

    Config in separate file

---
+++ src/DBconfig.js
@@ -0,0 +1,9 @@
+export const DB_CONFIG = {
+  apiKey: "AIzaSyARYN2XNweVC6TR-hePJ1yXgLiBkQTgn3U",
+  authDomain: "radical-display.firebaseapp.com",
+  databaseURL: "https://radical-display.firebaseio.com",
+  projectId: "radical-display",
+  storageBucket: "radical-display.appspot.com",
+  messagingSenderId: "515210546131",
+  appId: "1:515210546131:web:25bb98b16bd7f6091a3d26"
+};
