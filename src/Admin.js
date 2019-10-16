316ce8bae59b602b01e18a3304cdef56d12a4701
Frederik De Bleser <frederik@debleser.be>
2019-10-16 11:22:22 +0200

    Integrate upload and submit step in admin.

--- src/Admin.js
+++ src/Admin.js
@@ -5,96 +5,84 @@ import firebase from "firebase/app";
 import "firebase/storage";
 import "firebase/firestore";
 
+const FILE_TYPES = {
+  png: "image",
+  jpg: "image",
+  jpeg: "image",
+  gif: "image",
+  mp4: "movie"
+};
+
 export default class Admin extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { bucket: "test", type: "", message: "" };
-    this.sendToDB = this.sendToDB.bind(this);
-    this.storeFile = this.storeFile.bind(this);
-    this.storeType = this.storeType.bind(this);
+    this.state = { message: "", uploadProgress: 0 };
     this.db = firebase.firestore();
-    //this.ref = this.db.ref("/queue");
-    //console.log(this.ref);
+    this._onSelectFile = this._onSelectFile.bind(this);
   }
 
-  storeFile(e) {
-    let uplo = document.querySelector("#uploader");
+  _onSelectFile(e) {
     const file = e.target.files[0];
-    this.setState({
-      bucket: String(file.name),
-      type: ""
-    });
-    const storageRef = firebase.storage().ref(file.name);
-    let task = storageRef.put(file);
-
-    var thisParent = this;
+    const filename = file.name;
+    const fileExt = filename
+      .substring(filename.lastIndexOf(".") + 1)
+      .toLowerCase();
+    const fileType = FILE_TYPES[fileExt];
+    if (!fileType) {
+      this.setState({ message: "Unknown file type" });
+      return;
+    }
+    const storageRef = firebase.storage().ref(filename);
+    const task = storageRef.put(file);
     task.on(
       "state_changed",
-      function progress(snapshot) {
-        let perc = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        uplo.value = perc;
+      snapshot => {
+        const uploadProgress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        this.setState({ uploadProgress });
       },
-      null,
-      function(snapshot) {
-        //console.log("upload complete!");
-        console.log(storageRef.getDownloadURL());
-        storageRef.getDownloadURL().then(function(url) {
-          console.log(url);
-          thisParent.setState({
-            bucket: url
-          });
+      error => {
+        this.setState({
+          message: "Error while uploading. Is your file too big?"
         });
+      },
+      snapshot => {
+        console.log("storageRef", storageRef);
+        this.setState({ message: "Upload completed. " });
+        this._sendToDB(fileType, storageRef);
       }
     );
   }
 
-  storeType(e) {
-    var d = document.querySelector("select");
-    this.setState({
-      type: d.options[d.selectedIndex].value
-    });
-  }
-
-  sendToDB() {
-    var obj = { type: this.state.type, url: this.state.bucket };
-    console.log(obj);
+  async _sendToDB(fileType, storageRef) {
+    const fileUrl = await storageRef.getDownloadURL();
+    const obj = { type: fileType, url: fileUrl };
+    console.log("_sendToDB", obj);
     this.db
       .collection("queue")
       .add(obj)
       .then(docRef => {
-        console.log(docRef.id);
+        console.log("docRef", docRef);
+        this.setState({ message: "//// Screen successfully hacked //// " });
       })
       .catch(error => {
-        console.log(error);
+        this.setState({ message: "Error while creating database entry" });
       });
-
-    //.database()
-    //.ref("/queue")
-    //.push(obj);
   }
 
   render() {
+    const { uploadProgress, message } = this.state;
     return (
       <div className="noise">
         <div className="container">
           <h3>Add image/video</h3>
           <div className="form">
-            <input type="file" id="file" onChange={this.storeFile} />
+            <input type="file" id="file" onChange={this._onSelectFile} />
             <br />
-            <progress value="5" max="100" id="uploader">
+            <progress value="{ uploadProgress }" max="100" id="uploader">
               0%
             </progress>
-            <p className="message">{this.state.message}</p>
-            <br />
-            <label id="lab" htmlFor="sel">
-              type:
-            </label>
-            <select id="sel" onChange={this.storeType}>
-              <option value="image">image</option>
-              <option value="video">video</option>
-            </select>
-            <br />
-            <button onClick={this.sendToDB}>send</button>
+            <p className="message">{message}</p>
           </div>
         </div>
       </div>
